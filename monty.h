7dc5fc0671608cd0b831a2dfc05fddb2976a8a6e
MaxiHBTN <4387@holbertonschool.com>
2022-05-10 19:37:53 +0000

    Betty Style

--- monty.h
+++ monty.h
@@ -1,5 +1,5 @@
 #ifndef MONTY_H
-#define MONTY_H 
+#define MONTY_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,7 +8,7 @@
 
 extern int number;
 
-/*
+/**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
  * @prev: points to the previous element of the stack (or queue)
@@ -16,13 +16,13 @@ extern int number;
  *
  * Description: doubly linked list node structure
  * for stack, queues, LIFO, FIFO
- */
+ **/
 
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /**
@@ -31,12 +31,12 @@ typedef struct stack_s
  * @f: function to handle the opcode
  * Description: opcode and its function
  * for stack, queues, LIFO, FIFO
- */
+ **/
 
 typedef struct instruction_s
 {
-	        char *opcode;
-	        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 void push(stack_t **stack, unsigned int line_number __attribute__((unused)));
