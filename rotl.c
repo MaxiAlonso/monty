5c6934ce739c1411373ea3c83513b0b84e641d04
MaxiHBTN <4387@holbertonschool.com>
2022-05-11 21:02:29 +0000

    monty

--- rotl.c
+++ rotl.c
@@ -8,33 +8,5 @@
 
 void rotl(stack_t **stack, unsigned int line_number __attribute__((unused)))
 {
-	stack_t *new_node, *last_node;
-
-	if (*stack != NULL && (*stack)->next != NULL)
-	{
-		new_node = malloc(sizeof(stack_t));
-
-		if (new_node == NULL)
-		{
-			return (NULL);
-		}
-		new_node->n = n;
-		if (*head == NULL)
-		{
-			new_node->prev = NULL;
-			new_node->next = NULL;
-			*head = new_node;
-		}
-		else
-		{
-			last_node = *head;
-			while (last_node->next != NULL)
-			{
-				last_node = last_node->next;
-			}
-			last_node->next = new_node;
-			new_node->prev = last_node;
-			new_node->next = NULL;
-		}
-	}
+	(void) stack;
 }
