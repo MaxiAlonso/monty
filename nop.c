834aed733795c8e0e1004bae731855ad7c929ab9
MaxiHBTN <4387@holbertonschool.com>
2022-05-11 14:22:38 +0000

    Task 5

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * nop - doesn’t do anything.
+ * @stack: doubly linked list node structure
+ * @line_number: number of line wich contain the instruction..
+ **/
+
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
