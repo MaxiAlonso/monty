7c3c7a307418006ce42cc76c3710832540198b69
MaxiHBTN <4387@holbertonschool.com>
2022-05-11 19:05:46 +0000

    monty

--- pchar.c
+++ pchar.c
@@ -10,7 +10,7 @@ void pchar(stack_t **stack, unsigned int line_number)
 {
 	if (*stack != NULL)
 	{
-		if ((*stack)->n >= 0 && (*stack)->n <= 255)
+		if ((*stack)->n >= 0 && (*stack)->n <= 127)
 		{
 			printf("%c\n", (*stack)->n);
 		}
