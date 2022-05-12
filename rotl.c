d0c3fe54607ec58852a8eccdb359746197a37123
MaxiHBTN <4387@holbertonschool.com>
2022-05-12 12:44:07 +0000

    monty

--- rotl.c
+++ rotl.c
@@ -10,22 +10,15 @@ void rotl(stack_t **stack, unsigned int line_number)
 {
 	stack_t *new_node, *last_node;
 
-	new_node = malloc(sizeof(stack_t));
-	if (new_node == NULL)
+	if (*stack != NULL && (*stack)->next != NULL)
 	{
-		fprintf(stderr, "Error: malloc failed\n");
-		free_stack(*stack);
-		exit(EXIT_FAILURE);
-	}
-	new_node->n = (*stack)->n;
-	if (*stack == NULL)
-	{
-		new_node->prev = NULL;
-		new_node->next = NULL;
-		*stack = new_node;
-	}
-	else
-	{
+		new_node = malloc(sizeof(stack_t));
+		if (new_node == NULL)
+		{
+			fprintf(stderr, "Error: malloc failed\n");
+			exit(EXIT_FAILURE);
+		}
+		new_node->n = (*stack)->n;
 		last_node = *stack;
 		while (last_node->next != NULL)
 		{
@@ -34,7 +27,6 @@ void rotl(stack_t **stack, unsigned int line_number)
 		last_node->next = new_node;
 		new_node->prev = last_node;
 		new_node->next = NULL;
+		pop(stack, line_number);
 	}
-	pop(stack, line_number);
 }
-
